a78165e29cb0ba4d473bdfe057e10b94186aa896
MatiasPrietoHernan <matiasprietohernan@gmail.com>
2025-01-17 10:34:56 -0300

    New folders and functions

---
+++ CapaDatos/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        public Task<IEnumerable<T>> GetAllasync();
+        public Task<T> GetByIdAsync(int id);
+        public Task AddAsync(T entity);
+        public Task UpdateAsync(T entity);
+        public Task DeleteAsync(int id);
+
+    }
+}
--- CapaPresentación/Program.cs
+++ CapaPresentación/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using CapaDatos;
+
 namespace CapaPresentación
 {
     internal static class Program
@@ -5,13 +9,36 @@ namespace CapaPresentación
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        /// Este método se encarga de inicializar la aplicación.
+        private static IServiceProvider ServiceProvider;
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            // Aqui vamos a configurar para la inyección de dependencias
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(ServiceProvider.GetRequiredService<Form1>());
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlite("Data Source=CintaRoja.sqlite");
+            });
+
+            //Registrar servicios
+            // RegisterServices(services); Desenmarcar esto cuando se hayan registrado los servicios.
+
+            //Registrar los formularios. Todos los formularios que hagamos se tienen que registrar.
+            services.AddTransient<Form1>();
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            //Aqui se van a registrar los servicios, repositorios y demás.
         }
     }
 }
